43ce99da12f32e656433d05debd7d651170f2301
Daniel Fiala <freelancer@meave.biz>
2017-05-23 08:01:35 +0200

    JS demo reimplemented with protobuf.js, added response message and submessages are declared inside Request/Response message.

--- rpc.py
+++ rpc.py
@@ -3,32 +3,55 @@ import websockets
 
 import config
 import logging
+import traceback
 
 import gen.py.rpc_pb2 as rpc_messages
 
 logger = logging.getLogger('indexer.rpc')
 
 
+class RpcError(Exception):
+    def whoami(self):
+        return self.__class__.__name__
+
+class RpcNotImplementedError(RpcError):
+    pass
+
+class RpcUnknownRequest(RpcError):
+    pass
+
+class RpcEmptyRequest(RpcError):
+    pass
+
 async def _handler(wssp, req_uri):
     import datetime
     import random
-    logger.debug('Request uri: %s', req_uri)
-    while True:
-        request = rpc_messages.Request()
-        bytes = await wssp.recv()
-        request.ParseFromString(bytes)
-        logger.debug('Received: %s', request)
-        rtype = request.WhichOneof('type')
-        if rtype is None:
-            pass
-        elif (rtype == 'get_chain_info'):
-            pass
-        elif (rtype == 'get_tx'):
-            pass
-        elif (rtype == 'subscribe_addr'):
-            pass
-        else:
-            pass
+    logger.debug('R:%s L:%s Established connection: path:%s', wssp.remote_address, wssp.local_address, req_uri)
+    try:
+        while True:
+            request = rpc_messages.Request()
+            response = rpc_messages.Response()
+            bytes = await wssp.recv()
+            request.ParseFromString(bytes)
+            logger.debug("R:%s L:%s Received request:%s", wssp.remote_address, wssp.local_address, request)
+            rtype = request.WhichOneof('request')
+            try:
+                if rtype is None:
+                    raise RpcEmptyRequest
+                elif (rtype == 'get_chain_info'):
+                    raise RpcNotImplementedError
+                elif (rtype == 'get_tx'):
+                    raise RpcNotImplementedError
+                elif (rtype == 'subscribe_addr'):
+                    raise RpcNotImplementedError
+                else:
+                    raise RpcUnknownRequest
+            except RpcError as e:
+                response.error.msg = e.whoami()
+            logger.debug("R:%s L:%s Sending msg:%r", wssp.remote_address, wssp.local_address, response.SerializeToString())
+            await wssp.send(response.SerializeToString())
+    except websockets.exceptions.ConnectionClosed:
+        logger.info("R:%s L:%s The remote side has closed the connection", wssp.remote_address, wssp.local_address)
 
 async def init_server():
     server = await websockets.server.serve(_handler, config.server().host, config.server().port)
